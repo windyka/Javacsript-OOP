a7fa411234f11fdb1a41b473855baad835157c28
windyka <officialwindyka@gmail.com>
2019-01-12 00:17:54 +0700

    4- Constructors

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  draw: function() {
-    console.log('Draw');
-  }
-};
+// Factory Functions -------------------------
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log('Draw');
+    }
+  };
+}
 
-circle.draw(); // Output => Draw
+const circle = createCircle(1);
+// ------------------------------------------
+
+// Constructor Functions --------------------
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log('Draw!');
+  };
+}
+
+const another = new Circle(1);
+// write in console: Circle // output
+// ƒ Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function() {
+//       console.log('Draw!');
+//     };
+//   }
