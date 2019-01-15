bbe84c8cd4e8c81d2c94e83bd4f0b57931d3c524
windyka <officialwindyka@gmail.com>
2019-01-15 23:40:41 +0700

    5- Constructor Prototypes

--- index.js
+++ index.js
@@ -1,56 +1,26 @@
-let x = [];
+// Object.getPrototypeOf(myObj);
 
-console.log(x);
-// output
-// []
-// length: 0
-// __proto__: Array(0)
-// concat: ƒ concat()
-// constructor: ƒ Array()
-// copyWithin: ƒ copyWithin()
-// entries: ƒ entries()
-// every: ƒ every()
-// fill: ƒ fill()
-// filter: ƒ filter()
-// find: ƒ find()
-// findIndex: ƒ findIndex()
-// flat: ƒ flat()
-// flatMap: ƒ flatMap()
-// forEach: ƒ forEach()
-// includes: ƒ includes()
-// indexOf: ƒ indexOf()
-// join: ƒ join()
-// keys: ƒ keys()
-// lastIndexOf: ƒ lastIndexOf()
-// length: 0
-// map: ƒ map()
-// pop: ƒ pop()
-// push: ƒ push()
-// reduce: ƒ reduce()
-// reduceRight: ƒ reduceRight()
-// reverse: ƒ reverse()
-// shift: ƒ shift()
-// slice: ƒ slice()
-// some: ƒ some()
-// sort: ƒ sort()
-// splice: ƒ splice()
-// toLocaleString: ƒ toLocaleString()
-// toString: ƒ toString()
-// unshift: ƒ unshift()
-// values: ƒ values()
-// Symbol(Symbol.iterator): ƒ values()
-// Symbol(Symbol.unscopables): {copyWithin: true, entries: true, fill: true, find: true, findIndex: true, …}
-// __proto__:
-// constructor: ƒ Object()
-// hasOwnProperty: ƒ hasOwnProperty()
-// isPrototypeOf: ƒ isPrototypeOf()
-// propertyIsEnumerable: ƒ propertyIsEnumerable()
-// toLocaleString: ƒ toLocaleString()
-// toString: ƒ toString()
-// valueOf: ƒ valueOf()
-// __defineGetter__: ƒ __defineGetter__()
-// __defineSetter__: ƒ __defineSetter__()
-// __lookupGetter__: ƒ __lookupGetter__()
-// __lookupSetter__: ƒ __lookupSetter__()
-// get __proto__: ƒ __proto__()
-// set __proto__: ƒ __proto__()
+// myObj.__proto__(parent of myObj)
+// same as
+// Constructor.prototype()
+
+function Circle(radius) {
+  this.radius = radius;
+}
+
+Circle.prototype;
+
+const circle = new Circle(1);
+
+// write in the console:
+// -----------------------------
+// circle
+// Circle {radius: 1}
+// radius: 1
+// __proto__: Object
+// -----------------------------
+// Circle.prototype
+// {constructor: ƒ}
+// constructor: ƒ Circle(radius)
+// __proto__: Object
+// -----------------------------
