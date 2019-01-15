396654a18ca29cbc4b1bfd1ec9972ec761882be6
windyka <officialwindyka@gmail.com>
2019-01-15 20:45:22 +0700

    3- Multilevel Inheritance

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-let x = {};
+let x = [];
 
 console.log(x);
 // output
-// {}
+// []
+// length: 0
+// __proto__: Array(0)
+// concat: ƒ concat()
+// constructor: ƒ Array()
+// copyWithin: ƒ copyWithin()
+// entries: ƒ entries()
+// every: ƒ every()
+// fill: ƒ fill()
+// filter: ƒ filter()
+// find: ƒ find()
+// findIndex: ƒ findIndex()
+// flat: ƒ flat()
+// flatMap: ƒ flatMap()
+// forEach: ƒ forEach()
+// includes: ƒ includes()
+// indexOf: ƒ indexOf()
+// join: ƒ join()
+// keys: ƒ keys()
+// lastIndexOf: ƒ lastIndexOf()
+// length: 0
+// map: ƒ map()
+// pop: ƒ pop()
+// push: ƒ push()
+// reduce: ƒ reduce()
+// reduceRight: ƒ reduceRight()
+// reverse: ƒ reverse()
+// shift: ƒ shift()
+// slice: ƒ slice()
+// some: ƒ some()
+// sort: ƒ sort()
+// splice: ƒ splice()
+// toLocaleString: ƒ toLocaleString()
+// toString: ƒ toString()
+// unshift: ƒ unshift()
+// values: ƒ values()
+// Symbol(Symbol.iterator): ƒ values()
+// Symbol(Symbol.unscopables): {copyWithin: true, entries: true, fill: true, find: true, findIndex: true, …}
 // __proto__:
 // constructor: ƒ Object()
 // hasOwnProperty: ƒ hasOwnProperty()
@@ -17,27 +54,3 @@ console.log(x);
 // __lookupSetter__: ƒ __lookupSetter__()
 // get __proto__: ƒ __proto__()
 // set __proto__: ƒ __proto__()
-
-console.log(Object.getPrototypeOf(x));
-// output
-// {constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, …}
-// constructor: ƒ Object()
-// hasOwnProperty: ƒ hasOwnProperty()
-// isPrototypeOf: ƒ isPrototypeOf()
-// propertyIsEnumerable: ƒ propertyIsEnumerable()
-// toLocaleString: ƒ toLocaleString()
-// toString: ƒ toString()
-// valueOf: ƒ valueOf()
-// __defineGetter__: ƒ __defineGetter__()
-// __defineSetter__: ƒ __defineSetter__()
-// __lookupGetter__: ƒ __lookupGetter__()
-// __lookupSetter__: ƒ __lookupSetter__()
-// get __proto__: ƒ __proto__()
-// set __proto__: ƒ __proto__()
-
-console.log(x.toString());
-// output
-// [object Object]
-
-// A Prototype are just regular object
-// Every object has prototype or a parent, except the Root Object
