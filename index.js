1017994a61acb2f218a98888a68231d70dd55903
windyka <officialwindyka@gmail.com>
2019-01-16 00:36:52 +0700

    10- Exercise (Continuing Stopwatch Exercise)

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-function Circle(radius) {
-  this.radius = radius;
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
 
-  this.move = function() {
-    this.draw();
-    console.log('move');
+  this.start = function() {
+    if (running) throw new Error('Stopwatch has already started.');
+
+    running = true;
+
+    startTime = new Date();
   };
+
+  this.stop = function() {
+    if (!running) throw new Error('Stopwatch is not started.');
+
+    running = false;
+
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+
+  this.reset = function() {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+
+  Object.defineProperty(this, 'duration', {
+    get: function() {
+      return duration;
+    }
+  });
 }
-
-const c1 = new Circle(1);
-
-// Prototype member
-Circle.prototype.draw = function() {
-  console.log('draw');
-};
-
-// Return instance member
-console.log(Object.keys(c1));
-
-// Return all members
-for (let key in c1) console.log(key);
-
-// so the Prototype member doesn't have the own property
