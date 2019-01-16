03779bdfb71d37c1275c2e3e1ed2fd3ee2e58521
windyka <officialwindyka@gmail.com>
2019-01-16 16:54:59 +0700

    8- Mixins

--- index.js
+++ index.js
@@ -1,39 +1,111 @@
-function extend(Child, Parent) {
-  Child.prototype = Object.create(Parent.prototype);
-  Child.prototype.constructor = Child;
+// Step 1 ---------------------------------------------
+
+// const canEat = {
+//   eat: function() {
+//     this.hunger--;
+//     console.log('eating');
+//   }
+// };
+
+// const canWalk = {
+//   walk: function() {
+//     console.log('walking');
+//   }
+// };
+
+// function Person() {}
+
+// Object.assign(Person.prototype, canEat, canWalk);
+// const person = new Person();
+// console.log(person);
+// output
+// Person {}
+// __proto__:
+// eat: ƒ ()
+// walk: ƒ ()
+// constructor: ƒ Person()
+// __proto__: Object
+// --------------------------------------------------
+
+// Step 2 -------------------------------------------
+
+// const canEat = {
+//   eat: function() {
+//     this.hunger--;
+//     console.log('eating');
+//   }
+// };
+
+// const canWalk = {
+//   walk: function() {
+//     console.log('walking');
+//   }
+// };
+
+// const canSwim = {
+//   swim: function() {
+//     console.log('swim');
+//   }
+// };
+
+// function Person() {}
+
+// Object.assign(Person.prototype, canEat, canWalk);
+
+// const person = new Person();
+// console.log(person);
+
+// function Goldfish() {}
+
+// Object.assign(Goldfish.prototype, canEat, canSwim);
+
+// const goldfish = new Goldfish();
+// console.log(goldfish);
+// // Output
+// // Person {}
+// // Goldfish {}
+// ----------------------------------------------------
+
+// Step 3 (Mixin) -------------------------------------
+
+function mixin(target, ...sources) {
+  Object.assign(target, ...sources);
 }
 
-function Shape() {}
-
-Shape.prototype.duplicate = function() {
-  console.log('duplicate');
+const canEat = {
+  eat: function() {
+    this.hunger--;
+    console.log('eating');
+  }
 };
 
-function Circle() {}
-
-extend(Circle, Shape);
-
-Circle.prototype.duplicate = function() {
-  Shape.prototype.duplicate.call(this);
-
-  console.log('duplicate circle');
+const canWalk = {
+  walk: function() {
+    console.log('walking');
+  }
 };
 
-function Square() {}
-
-extend(Square, Shape);
-
-Square.prototype.duplicate = function() {
-  Shape.prototype.duplicate.call(this);
-
-  console.log('duplicate square');
+const canSwim = {
+  swim: function() {
+    console.log('swim');
+  }
 };
 
-const shapes = [new Circle(), new Square()];
+function Person() {}
 
-for (let shape of shapes) shape.duplicate();
-// output
-// duplicate
-// duplicate circle
-// duplicate
-// duplicate square
+mixin(Person.prototype, canEat, canWalk);
+
+const person = new Person();
+console.log(person);
+
+function Goldfish() {}
+
+mixin(Goldfish.prototype, canEat, canSwim);
+
+const goldfish = new Goldfish();
+console.log(goldfish);
+// console.log(goldfish);
+// // Output
+// // Person {}
+// // Goldfish {}
+// ----------------------------------------------------
