5d82952e79271cb4a96d2a059ae1545fd7b54d56
windyka <officialwindyka@gmail.com>
2019-01-16 15:32:46 +0700

    5- Method Overriding

--- index.js
+++ index.js
@@ -1,48 +1,27 @@
-function Shape(color) {
-  this.color = color;
-}
-
-Shape.prototype.duplicate = function() {
-  console.log('duplicate');
-};
-
 function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 }
 
-function Circle(radius, color) {
-  Shape.call(this, color);
+function Shape() {}
 
-  this.radius = radius;
-}
+Shape.prototype.duplicate = function() {
+  console.log('duplicate');
+};
+
+function Circle() {}
 
 extend(Circle, Shape);
 
-Circle.prototype.draw = function() {
-  console.log('draw');
+Circle.prototype.duplicate = function() {
+  Shape.prototype.duplicate.call(this);
+
+  console.log('duplicate circle');
 };
 
-function Square(size) {
-  this.size = size;
-}
-
-extend(Square, Shape);
-
-Square.prototype = Object.create(Shape.prototype);
-Square.prototype.constructor = Circle;
-
-const s = new Shape();
-const c = new Circle(1, 'red');
-//  write in the console; -------------------
-// Step 1 -----------------------------------
-// c
-// Circle {color: "red", radius: 1}
-// ------------------------------------------
-// Step 2 -----------------------------------
-// const sq = new Square(10)
-// > undefined
-// sq.duplicate()
-// > duplicate
+const c = new Circle();
+// write in console;
+// c.duplicate()
+//   duplicate
+//   duplicate circle
 // undefined
-// ------------------------------------------
