ff2892290d3656375b81c96ea6d564e5f0a0498b
windyka <officialwindyka@gmail.com>
2019-01-17 04:22:06 +0700

    6- Private Members Using WeakMaps

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const _radius = Symbol();
-const _draw = Symbol();
+const _radius = new WeakMap();
+const _move = new WeakMap();
 
 class Circle {
   constructor(radius) {
-    this[_radius] = radius;
+    _radius.set(this, radius);
+
+    _move.set(this, () => {
+      console.log('move', this);
+    });
   }
-  [_draw]() {}
+
+  draw() {
+    _move.get(this)();
+
+    console.log('draw');
+  }
 }
 
 const c = new Circle(1);
-const key = Object.getOwnPropertySymbols(c)[0];
-console.log(c[key]);
-// write in console;
-// 1
-// c
-// Circle {Symbol(): 1}Symbol(): 1__proto__: constructor: class CircleSymbol(): ƒ [_draw]()__proto__: Object
+// output
+// c.draw()
+//  move Circle {}__proto__: Object
+//  draw
+// undefined
