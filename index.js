50a029e0be07b5fb9436e8e1b4c3f5bc593ca792
windyka <officialwindyka@gmail.com>
2019-01-16 14:56:01 +0700

    4- Intermediate Function Inheritance

--- index.js
+++ index.js
@@ -6,22 +6,43 @@ Shape.prototype.duplicate = function() {
   console.log('duplicate');
 };
 
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
 function Circle(radius, color) {
   Shape.call(this, color);
 
   this.radius = radius;
 }
 
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
+extend(Circle, Shape);
 
 Circle.prototype.draw = function() {
   console.log('draw');
 };
 
+function Square(size) {
+  this.size = size;
+}
+
+extend(Square, Shape);
+
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Circle;
+
 const s = new Shape();
 const c = new Circle(1, 'red');
-//  write in the console;
-
+//  write in the console; -------------------
+// Step 1 -----------------------------------
 // c
 // Circle {color: "red", radius: 1}
+// ------------------------------------------
+// Step 2 -----------------------------------
+// const sq = new Square(10)
+// > undefined
+// sq.duplicate()
+// > duplicate
+// undefined
+// ------------------------------------------
